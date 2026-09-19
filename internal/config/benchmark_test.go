5dc0f722c143745adb7469df9a4b8cb73e0ee530
aalsanie <ahmad.alsanie@hotmail.com>
2026-09-19 11:29:44 +0300

    measure config decoding directly

--- internal/config/benchmark_test.go
+++ internal/config/benchmark_test.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"strings"
+	"bytes"
 	"testing"
 )
 
@@ -11,7 +11,7 @@ func BenchmarkDecodeConfig(b *testing.B) {
 	b.ReportAllocs()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		if _, err := Decode(strings.NewReader(string(raw))); err != nil {
+			if _, err := Decode(bytes.NewReader(raw)); err != nil {
 			b.Fatal(err)
 		}
 	}
